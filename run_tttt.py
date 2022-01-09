b6f305ee6ee294fed9cb0dc9d1b5b50c806eb09b
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2022-01-08 19:28:24 -0800

    Update run_tttt.py
    I made an updated version which uses dictionaries and stacks like run_bf.

--- run_tttt.py
+++ run_tttt.py
@@ -1,9 +1,39 @@
 # This is based on a programming language.
-def run_tttt(data: str) -> None:
+def run_tttt(data: str, textfile: False) -> None:
     code = list(data)
+
+    if textfile:
+        code = ''.join(open(f'{code}', 'r').readlines())
+
     variables = [0, 0, 0]
     pointer = 0
     which_var = 0
+    input_string = ''  # allows multiple inputs to happen easily
+
+    corresponding_thing = {}  # dictionary for the corresponding beginning/ending things
+    loop_queue = []  # acts as a queue for the last loop
+    comment_queue = []  # acts as a queue for the last comment
+    in_comment = False
+    for num, char in enumerate(code):
+        if char == 'k':
+            in_comment = True
+            comment_queue.append(num)
+        elif char == 'l':
+            assert len(comment_queue) > 0, 'unmatched l'
+            corresponding_thing[num] = comment_queue[0]
+            corresponding_thing[comment_queue[0]] = num
+            comment_queue = comment_queue[1:]
+            in_comment = False
+        elif not in_comment:
+            if char == 'i':
+                loop_queue.append(num)
+            elif char == 'j':
+                assert len(loop_queue) > 0, 'unmatched j'
+                corresponding_thing[num] = loop_queue[0]
+                corresponding_thing[loop_queue[0]] = num
+                loop_queue = loop_queue[1:]
+    assert len(loop_queue) == 0, 'unmatched i'
+    assert len(comment_queue) == 0, 'unmatched k'
 
     while pointer < len(code):
         if 'a' == code[pointer]:
@@ -21,22 +51,20 @@ def run_tttt(data: str) -> None:
         elif 'g' == code[pointer]:
             print("\n", end="")
         elif 'h' == code[pointer]:
-            variables[which_var] = ord(input())
+            if input_string == '':
+                input_string = input(">>")
+            if len(input_string) > 0:
+                variables[which_var] = ord(input_string[0])
+            else:
+                variables[which_var] = 10
+            input_string = input_string[1:]
         elif 'i' == code[pointer]:
             if variables[which_var] == 0:
-                while code[pointer] != 'j':
-                    if code[pointer] == 'k':
-                        pointer += code[pointer:].index('l')
-                    pointer += 1
+                pointer = corresponding_thing[pointer]
         elif 'j' == code[pointer]:
             if variables[which_var] != 0:
-                while code[pointer] != 'i':
-                    if code[pointer] == 'l':
-                        # I don't know how to best use index here
-                        while code[pointer] != 'k':
-                            pointer -= 1
-                    pointer -= 1
-                pointer -= 1
+                pointer = corresponding_thing[pointer]
         elif 'k' == code[pointer]:
-            pointer += code[pointer:].index('l')
+            pointer = corresponding_thing[pointer]
+        assert 0 <= which_var <= 2, "the pointer must always be between 0 and 2, inclusive"
         pointer += 1
