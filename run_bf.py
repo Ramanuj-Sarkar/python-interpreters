2c7f120f2968769433b9c51faf2e92ce9788f7d6
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2022-06-05 09:17:45 -0700

    Update run_bf.py
    Fixed something which could end up bothering people.

--- run_bf.py
+++ run_bf.py
@@ -1,14 +1,14 @@
-def run_bf(code: str, textfile = False) -> None:
+def run_bf(code: str, textfile=False) -> None:
     pointer = 0     # for instructions
     location = 0    # for tape
     tape = [0]
     input_string = ''  # allows multiple inputs to happen easily
-
+    
     if textfile:
         code = ''.join(open(f'{code}', 'r').readlines())
-
     corresponding_bracket = {}       # dictionary where the values are the corresponding bracket positions of the keys
     bracket_stack = []     # acts as a stack for the last bracket
+    
     for num, char in enumerate(code):
         if char == '[':
             bracket_stack.append(num)
@@ -18,17 +18,15 @@ def run_bf(code: str, textfile = False) -> None:
             corresponding_bracket[bracket_stack[-1]] = num
             bracket_stack.pop()
     assert len(bracket_stack) == 0, 'unmatched ['
-
+    
     while pointer < len(code):
         if code[pointer] == '>':
             location += 1
             if location == len(tape):
                 tape.append(0)
         elif code[pointer] == '<':
+            assert location > 0, 'Cannot move left from position 0'
             location -= 1
-            if location < 0:
-                tape = [0] + tape
-                location += 1
         elif code[pointer] == '+':
             tape[location] = (tape[location] + 1) % 256
         elif code[pointer] == '-':
@@ -40,9 +38,9 @@ def run_bf(code: str, textfile = False) -> None:
                 input_string = input(">>")
             if len(input_string) > 0:
                 tape[location] = ord(input_string[0])
+                input_string = input_string[1:]
             else:
                 tape[location] = 10
-            input_string = input_string[1:]
         elif code[pointer] == '[':
             if tape[location] == 0:
                 pointer = corresponding_bracket[pointer]
