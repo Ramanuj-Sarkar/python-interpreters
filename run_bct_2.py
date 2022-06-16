c082d83423fe14362e67297a2b7f54ed63e7688e
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2022-06-15 23:37:46 -0700

    Create run_bct_2.py
    I made this now.

---
+++ run_bct_2.py
@@ -0,0 +1,31 @@
+def run_bct_2(program: str, data: str) -> None:
+    # Will not operate on strings which contain non-bits
+    assert set(program).issubset({'0','1'}),\
+        "The input strings must contain only 0 and 1. The program string contains at least one illegal bit."
+    assert set(data).issubset({'0', '1'}),\
+        "The input strings must contain only 0 and 1. The data string contains at least one illegal bit."
+    # padding for print statements
+    padding = ""
+    # allows you to step through the program
+    # and quickly stop infinite loops
+    stop = ""
+    # if data is empty, it stops
+    # if input is not empty, it stops
+    print('Commands|Data')
+    while data != "" and stop == "":
+        if program[0] == "0":
+            # prints output similar to esolangs page
+            print(f"    {program[0]}   |{padding}{data}", end="")
+            program = program[1:] + program[:1]
+            padding += " "
+            data = data[1:]
+        else:
+            print(f"   {program[:2]}   |{padding}{data}", end="")
+            if data[0] == "1":
+                data += program[1]
+            program = program[2:] + program[:2]
+        stop = input()
+
+
+if __name__ == '__main__':
+    run_bct('1111', '1')
