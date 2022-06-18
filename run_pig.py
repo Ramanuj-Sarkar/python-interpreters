8ec01c41960f6d75a0419f4b52bc95771d833299
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2022-06-17 21:58:33 -0700

    Update run_pig.py
    Added those type things.

--- run_pig.py
+++ run_pig.py
@@ -1,5 +1,5 @@
 # runs language from https://esolangs.org/wiki/Pig
-def run_pig(code):
+def run_pig(code: str) -> None:
     assert type(code) is str, 'ERROR: Code must be a string.'
     assert 'PIG' in code, 'ERROR: Code must contain "PIG".'
     file_name, file_text = code.split('PIG',1)
