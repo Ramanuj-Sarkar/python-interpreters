bf1d07cf45c4497abcdcaa9fbe0968f1d11ede37
Ramanuj Sarkar <66799041+Ramanuj-Sarkar@users.noreply.github.com>
2023-11-01 17:53:55 -0400

    Update run_bf.py
    The new one is better.

--- run_bf.py
+++ run_bf.py
@@ -1,15 +1,15 @@
 # runs language from https://esolangs.org/wiki/Brainfuck
-def run_bf(code: str, textfile=False) -> None:
-    pointer = 0     # for instructions
-    location = 0    # for tape
+def run_bf(code: str, textfile=False, starting_input='') -> None:
+    pointer = 0  # for instructions
+    location = 0  # for tape
     tape = [0]
-    input_string = ''  # allows multiple inputs to happen easily
-    
+    input_string = starting_input  # allows multiple inputs to happen easily
+
     if textfile:
         code = ''.join(open(f'{code}', 'r').readlines())
-    corresponding_bracket = {}       # dictionary where the values are the corresponding bracket positions of the keys
-    bracket_stack = []     # acts as a stack for the last bracket
-    
+    corresponding_bracket = {}  # dictionary where the values are the corresponding bracket positions of the keys
+    bracket_stack = []  # acts as a stack for the last bracket
+
     for num, char in enumerate(code):
         if char == '[':
             bracket_stack.append(num)
@@ -19,7 +19,7 @@ def run_bf(code: str, textfile=False) -> None:
             corresponding_bracket[bracket_stack[-1]] = num
             bracket_stack.pop()
     assert len(bracket_stack) == 0, 'unmatched ['
-    
+
     while pointer < len(code):
         if code[pointer] == '>':
             location += 1
@@ -33,7 +33,7 @@ def run_bf(code: str, textfile=False) -> None:
         elif code[pointer] == '-':
             tape[location] = (tape[location] - 1) % 256
         elif code[pointer] == '.':
-            print(chr(tape[location]),end='')
+            print(chr(tape[location]), end='')
         elif code[pointer] == ',':
             if input_string == '':
                 input_string = input(">>")
